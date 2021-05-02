e4b4d6c6ee60f374f2c56b028947b9cf08c4a071
Dhruv Shrivastava <dhruvsri5@gmail.com>
2021-05-02 23:40:38 +0530

    c

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'f7u08nwwko+p2b#d!6tl-11nzq*b73cs-^m6#k=3i!ew@ckw@a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '0.0.0.0',
+    'django-calculator-dhruv.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
